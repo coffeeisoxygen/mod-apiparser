4737ea384c85713ade06cbcd306c965e68d0bcd0
Hasan Maki <hasanmaki.ahmad@gmail.com>
2025-08-06 22:24:49 +0700

    refactor: remove unused dependency module and clean imports
    This commit deletes the mod_depends.py file, which contained unused dependency injection functions for domain services. Additionally, it cleans up the imports in dep_auth.py to streamline the code.

--- src/dependencies/dep_auth.py
+++ src/dependencies/dep_auth.py
@@ -1,13 +1,14 @@
 """dependencies untuk admin."""
-from fastapi import Depends, HTTPException, status, Request
+
+from fastapi import Request
 from fastapi.security import OAuth2PasswordBearer
+
 from src.repos.rep_user import UserRepository
-from src.domain.user.sch_user import UserInDB
 
 # Skema keamanan dasar, bisa disesuaikan
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+
 def get_user_repo(request: Request) -> UserRepository:
     """Dependency injection untuk User Repository."""
     return request.app.state.user_repo
-
